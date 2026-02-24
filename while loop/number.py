87f9951056d02955e3b0947f599d013a6338ea3d
mickey <bhavanaedupalli07@gmail.com>
2026-02-24 16:45:14 +0530

    add file

---
+++ while loop/number.py
@@ -0,0 +1,14 @@
+while True:
+  a = int(input("Enter your marks"))
+  if a==-1:
+    break
+  elif a>=90:
+    print("Grade = A")
+  elif a>=75:
+    print("Grade = B")
+  elif a>=50:
+    print("Grade = C")
+  else:
+    print("Grade = F")
+
+
