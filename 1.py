11d7fde59795afafd95a3071cdbfa6dffffff31d
mickey <bhavanaedupalli07@gmail.com>
2026-02-24 16:09:33 +0530

    sum of odd

--- 1.py
+++ 1.py
@@ -1,6 +1,20 @@
+
+
+
+
+#sum of numbers from 1 to 20
 i=1
 sum=0
 while i<=20:
     sum+=i
     i+=1  #i=i+1
 print("The sum of numbers from 1 to 20 is",sum)
+
+#sum of odd numbers from 1 to 20
+i=1
+sum=0
+while i<=20:
+    if i%2 !=0:
+     sum+=i
+    i+=1
+print("The sum of all odd numbers from 1 to 20 is",sum)
